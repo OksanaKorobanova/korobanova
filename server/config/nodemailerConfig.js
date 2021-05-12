e4893e1af9b1c20ca8cd8c1be63ba9c66bf17555
Oksana.Korobanova <oksanavitol@gmail.com>
2021-05-12 21:49:43 +0300

    changed nodemailer config

--- server/config/nodemailerConfig.js
+++ server/config/nodemailerConfig.js
@@ -4,10 +4,9 @@ const nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    // user: process.env.REACT_APP_USER_FROM, // email address to send email from
-    // pass: process.env.REACT_APP_PASS // gmail account password
-    user: 'korobanovaweb@gmail.com', // email address to send email from
-    pass: 'd!n0km74269./123web' // gmail account password
+    user: process.env.REACT_APP_USER_FROM, // email address to send email from
+    pass: process.env.REACT_APP_PASS // gmail account password
+ 
   }
 });
 
