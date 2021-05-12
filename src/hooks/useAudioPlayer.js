d1e8d5ebc38b67291837039ede91f2a4544c1acc
Oksana.Korobanova <oksanavitol@gmail.com>
2021-05-12 22:00:03 +0300

    pdf-js-worker added

--- src/hooks/useAudioPlayer.js
+++ src/hooks/useAudioPlayer.js
@@ -1,31 +1,30 @@
 import { useState, useRef, useEffect } from 'react';
 
-
 export function useAudioPlayer(audioFile, volume = 1) {
   const [isPlaying, setIsPlaying] = useState(false);
 
   const audioRef = useRef(new Audio(audioFile));
   audioRef.current.volume = volume;
 
-  useEffect(() => {
-    if (isPlaying) {
-      audioRef.current.play();
-    } else {
-      audioRef.current.pause();
-    }
-  }, [isPlaying]);
-
-  // const handleChangePlayer = () => {
+  // useEffect(() => {
   //   if (isPlaying) {
-  //     setIsPlaying(false);
-  //     audioRef.current.pause();
-      
-  //   } else {
-  //     setIsPlaying(true);
   //     audioRef.current.play();
+  //   } else {
+  //     audioRef.current.pause();
   //   }
-  // }
-  const handleChangePlayer = () => setIsPlaying((prevState) => !prevState);
+  // }, [isPlaying]);
+  // const handleChangePlayer = () => setIsPlaying((prevState) => !prevState);
+
+  // for mobile browsers
+  const handleChangePlayer = () => {
+    if (isPlaying) {
+      setIsPlaying(false);
+      audioRef.current.pause();
+    } else {
+      setIsPlaying(true);
+      audioRef.current.play();
+    }
+  };
 
   return {
     isPlaying,
